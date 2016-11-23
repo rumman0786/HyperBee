0c92bf704c073c06ea2ba7acb33495603ab7bc05
Rimi Reza Duity <duity@therapservices.net>
2016-11-23 17:54:40 +0600

    string replaced with constant in profile controller

--- src/main/java/net/therap/hyperbee/web/controller/ProfileController.java
+++ src/main/java/net/therap/hyperbee/web/controller/ProfileController.java
@@ -5,31 +5,37 @@ import net.therap.hyperbee.domain.Profile;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
+
+import static net.therap.hyperbee.utils.constant.Url.PROFILE_URL;
+import static net.therap.hyperbee.utils.constant.Url.CREATE_PROFILE_URL;
 
 /**
  * @author duity
  * @since 11/22/16.
  */
 @Controller
+@RequestMapping(value = PROFILE_URL)
 public class ProfileController {
 
     @Autowired
     private ProfileDao profileDao;
 
-    @RequestMapping(value = "/profile", method = RequestMethod.GET)
+    @GetMapping
     public String getProfile(Model model){
         model.addAttribute("profile", new Profile());
 
-        return "profile/createprofile";
+        return CREATE_PROFILE_URL;
     }
 
-    @RequestMapping(value = "/profile", method = RequestMethod.POST)
-    public String postProfile(@ModelAttribute Profile profile){
-        profileDao.save(profile);
+    @PostMapping
+    public String postProfile(@ModelAttribute Profile profile, Model model){
+        String message=profileDao.save(profile);
+        model.addAttribute("message", message);
 
-        return "profile/createprofile";
+        return CREATE_PROFILE_URL;
     }
 }
