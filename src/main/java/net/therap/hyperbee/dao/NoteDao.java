7eab679498752cc52fc6085f662d5ae1b873c92a
bashir <bashir@therapservices.net>
2016-11-23 15:59:08 +0600

    Added dao methods for note view

--- src/main/java/net/therap/hyperbee/dao/NoteDao.java
+++ src/main/java/net/therap/hyperbee/dao/NoteDao.java
@@ -4,6 +4,8 @@ import net.therap.hyperbee.domain.Note;
 import net.therap.hyperbee.domain.User;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 /**
  * @author bashir
  * @since 11/22/16
@@ -20,4 +22,8 @@ public interface NoteDao {
     public void deleteById(int noteId);
 
     public void createNoteAndUser(Note note, User user);
+
+    public void saveNoteForUser(Note note, User user);
+
+    public List<Note> findActiveNoteListByUserId(int userId);
 }
--- src/main/java/net/therap/hyperbee/dao/NoteDaoImpl.java
+++ src/main/java/net/therap/hyperbee/dao/NoteDaoImpl.java
@@ -2,11 +2,13 @@ package net.therap.hyperbee.dao;
 
 import net.therap.hyperbee.domain.Note;
 import net.therap.hyperbee.domain.User;
+import net.therap.hyperbee.domain.enums.DisplayStatus;
 import org.springframework.stereotype.Repository;
 import org.springframework.transaction.annotation.Transactional;
 
 import javax.persistence.EntityManager;
 import javax.persistence.PersistenceContext;
+import java.util.List;
 
 /**
  * @author bashir
@@ -16,11 +18,13 @@ import javax.persistence.PersistenceContext;
 public class NoteDaoImpl implements NoteDao {
 
     @PersistenceContext
-    EntityManager entityManager;
+    EntityManager em;
 
     @Override
     public void create(Note note) {
 
+        em.persist(note);
+        em.flush();
     }
 
     @Override
@@ -42,7 +46,23 @@ public class NoteDaoImpl implements NoteDao {
     @Transactional
     public void createNoteAndUser(Note note, User user) {
         user.getNoteList().add(note);
-        entityManager.persist(user);
-        entityManager.flush();
+        em.persist(user);
+        em.flush();
+    }
+
+    @Override
+    @Transactional
+    public void saveNoteForUser(Note note, User user) {
+
+        user.getNoteList().add(note);
+        em.flush();
+    }
+
+    @Override
+    public List<Note> findActiveNoteListByUserId(int userId) {
+        return em.createNamedQuery("Note.findNoteByUserId", Note.class)
+                .setParameter("userId", userId)
+                .setParameter("displayStatus", DisplayStatus.ACTIVE)
+                .getResultList();
     }
 }
