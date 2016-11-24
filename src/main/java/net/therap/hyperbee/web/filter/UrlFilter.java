8741efbb065fdd7f6fa6afad2715c49796da7ce7
Md. Rayed Bin Wahed <rayed@therapservices.net>
2016-11-24 14:47:53 +0600

    Added url filter

---
+++ src/main/java/net/therap/hyperbee/web/filter/UrlFilter.java
@@ -0,0 +1,43 @@
+package net.therap.hyperbee.web.filter;
+
+import net.therap.hyperbee.web.helper.SessionHelper;
+import net.therap.hyperbee.web.security.AuthUser;
+
+import javax.servlet.*;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+import java.io.IOException;
+
+/**
+ * @author rayed
+ * @since 11/24/16 1:29 PM
+ */
+
+public class UrlFilter implements Filter {
+
+    @Override
+    public void init(FilterConfig filterConfig) throws ServletException {
+
+    }
+
+    @Override
+    public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain) throws IOException, ServletException {
+        HttpSession session = ((HttpServletRequest) request).getSession();
+        AuthUser authUser = SessionHelper.retrieveAuthUserFromSession(session);
+
+        if (authUser != null){
+            chain.doFilter(request, response);
+
+            return;
+        }
+
+        HttpServletResponse httpServletResponse = (HttpServletResponse) response;
+        httpServletResponse.sendRedirect("/login");
+    }
+
+    @Override
+    public void destroy() {
+
+    }
+}
