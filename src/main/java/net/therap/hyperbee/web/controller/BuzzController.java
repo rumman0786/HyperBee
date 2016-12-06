723ff486de91dc52f9587a25b70cf26de9ec02e5
Shamah Mahbub Zoha <zoha@therapservices.net>
2016-12-06 11:18:23 +0600

    Resolved hard-coded values to constants

--- src/main/java/net/therap/hyperbee/web/controller/BuzzController.java
+++ src/main/java/net/therap/hyperbee/web/controller/BuzzController.java
@@ -13,12 +13,12 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
-import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.WebDataBinder;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 import javax.servlet.http.HttpSession;
+import javax.validation.Valid;
 import java.util.List;
 
 import static net.therap.hyperbee.utils.constant.Url.BUZZ_BASE_URL;
@@ -33,12 +33,32 @@ import static net.therap.hyperbee.utils.constant.Url.USER_DASHBOARD_URL;
 
 public class BuzzController {
 
+    //URL Constants
     private static final String BUZZ_VIEW_URL = "/buzzList";
     private static final String BUZZ_CREATE_URL = "/sendBuzz";
     private static final String BUZZ_FLAG_URL = "/flagBuzz";
     private static final String BUZZ_DEACTIVATE_URL = "/deactivateBuzz";
     private static final String BUZZ_PIN_URL = "/pinBuzz";
     private static final String BUZZ_HISTORY_URL = "/buzzHistory";
+
+    // Log Message Constants
+    private static final String BUZZ_GET_LOG = "Passing Buzz Lists to view for displaying.";
+    private static final String BUZZ_SAVE_LOG = "Created new buzz and logged in activity log.";
+    private static final String BUZZ_FLAG_LOG = "Flagged buzz with id = {} and logged in activity log.";
+    private static final String BUZZ_REMOVE_LOG = "Deactivated buzz with id = {} and logged in activity log.";
+    private static final String BUZZ_PIN_LOG = "Pinned buzz with id = {} and logged in activity log.";
+    private static final String BUZZ_HISTORY_LOG = "Sending buzz list as per requirement for viewing history.";
+    private static final String BUZZ_ERROR_LOG = "Encountered an error. Propagating message to view.";
+
+    // Attribute alias and value Constants
+    private static final String BUZZ_LIST_ATTR_NAME = "buzzList";
+    private static final String BUZZ_LIST_SIZE_ATTR_NAME = "buzzListSize";
+    private static final String BUZZ_PINNED_LIST_ATTR_NAME = "pinnedBuzzList";
+    private static final String BUZZ_NEW_ATTR_NAME = "newBuzz";
+    private static final String BUZZ_PAGE_ATTR_NAME = "page";
+    private static final String BUZZ_PAGE_ATTR_VALUE = "buzz";
+    private static final String BUZZ_NEXT_ATTR_NAME = "next";
+    private static final String BUZZ_PREV_ATTR_NAME = "prev";
 
     private static final Logger log = LogManager.getLogger(BuzzController.class);
 
@@ -61,21 +81,21 @@ public class BuzzController {
 
     @GetMapping(BUZZ_VIEW_URL)
     public void viewLatestBuzz(Model model) {
-        model.addAttribute("pinnedBuzzList", buzzService.getLatestPinnedBuzz());
-        model.addAttribute("buzzList", buzzService.getLatestBuzz());
+        model.addAttribute(BUZZ_PINNED_LIST_ATTR_NAME, buzzService.getLatestPinnedBuzz());
+        model.addAttribute(BUZZ_LIST_ATTR_NAME, buzzService.getLatestBuzz());
 
-        log.debug("Passing Buzz Lists to view for displaying.");
+        log.debug(BUZZ_GET_LOG);
     }
 
     @PostMapping(BUZZ_CREATE_URL)
-    public String sendBuzz(@ModelAttribute("newBuzz") @Validated Buzz newBuzz, BindingResult result,
+    public String sendBuzz(@ModelAttribute(BUZZ_NEW_ATTR_NAME) @Valid Buzz newBuzz, BindingResult result,
                            RedirectAttributes redirectAttributes, HttpSession session, Model model) {
 
         if (result.hasErrors()) {
-            log.debug("Encountered an error. Propagating message to view.");
+            log.debug(BUZZ_ERROR_LOG);
 
-            redirectAttributes.addFlashAttribute(BindingResult.MODEL_KEY_PREFIX + "newBuzz", result);
-            redirectAttributes.addFlashAttribute("newBuzz", newBuzz);
+            redirectAttributes.addFlashAttribute(BindingResult.MODEL_KEY_PREFIX + BUZZ_NEW_ATTR_NAME, result);
+            redirectAttributes.addFlashAttribute(BUZZ_NEW_ATTR_NAME, newBuzz);
 
             return Utils.redirectTo(USER_DASHBOARD_URL);
         }
@@ -85,9 +105,9 @@ public class BuzzController {
         newBuzz.setUser(userService.findByUsername(authUser.getUsername()));
         buzzService.saveBuzz(newBuzz);
 
-        log.debug("Created new buzz and logged in activity log.");
+        log.debug(BUZZ_SAVE_LOG);
 
-        model.addAttribute("newBuzz", new Buzz());
+        model.addAttribute(BUZZ_NEW_ATTR_NAME, new Buzz());
 
         return Utils.redirectTo(USER_DASHBOARD_URL);
     }
@@ -95,7 +115,7 @@ public class BuzzController {
     @GetMapping(BUZZ_FLAG_URL)
     public String flagBuzz(int id) {
         Buzz flagBuzz = buzzService.flagBuzz(buzzService.getBuzzById(id));
-        log.debug("Flagged buzz with id = {} and logged in activity log.", id);
+        log.debug(BUZZ_FLAG_LOG, id);
 
         return Utils.redirectTo(USER_DASHBOARD_URL);
     }
@@ -103,7 +123,7 @@ public class BuzzController {
     @GetMapping(BUZZ_DEACTIVATE_URL)
     public String deactivateBuzz(int id) {
         Buzz deactivateBuzz = buzzService.deactivateBuzz(buzzService.getBuzzById(id));
-        log.debug("Deactivated buzz with id = {} and logged in activity log.", id);
+        log.debug(BUZZ_REMOVE_LOG, id);
 
         return Utils.redirectTo(USER_DASHBOARD_URL);
     }
@@ -111,28 +131,31 @@ public class BuzzController {
     @GetMapping(BUZZ_PIN_URL)
     public String pinBuzz(int id) {
         Buzz pinBuzz = buzzService.pinBuzz(buzzService.getBuzzById(id));
-        log.debug("Pinned buzz with id = {} and logged in activity log.", id);
+        log.debug(BUZZ_PIN_LOG, id);
 
         return Utils.redirectTo(USER_DASHBOARD_URL);
     }
 
     @GetMapping(BUZZ_HISTORY_URL)
-    public String viewBuzzHistory(@RequestParam("prev") int prev, @RequestParam("next") int next, Model model) {
+    public String viewBuzzHistory(@RequestParam(BUZZ_PREV_ATTR_NAME) int prev,
+                                  @RequestParam(BUZZ_NEXT_ATTR_NAME) int next, Model model) {
+
         List<Buzz> buzzList = buzzService.getAllBuzz();
+        model.addAttribute(BUZZ_LIST_SIZE_ATTR_NAME, buzzList.size());
 
         if (next > buzzList.size()) {
-            model.addAttribute("buzzList", buzzList.subList(prev, buzzList.size()));
+            model.addAttribute(BUZZ_LIST_ATTR_NAME, buzzList.subList(prev, buzzList.size()));
             next = buzzList.size();
         } else {
-            model.addAttribute("buzzList", buzzList.subList(prev, next));
+            model.addAttribute(BUZZ_LIST_ATTR_NAME, buzzList.subList(prev, next));
         }
 
-        model.addAttribute("prev", prev);
-        model.addAttribute("next", next);
+        model.addAttribute(BUZZ_PREV_ATTR_NAME, prev);
+        model.addAttribute(BUZZ_NEXT_ATTR_NAME, next);
 
-        model.addAttribute("page", "buzz");
+        model.addAttribute(BUZZ_PAGE_ATTR_NAME, BUZZ_PAGE_ATTR_VALUE);
 
-        log.debug("Sending buzz list as per requirement for viewing history.");
+        log.debug(BUZZ_HISTORY_LOG);
 
         return BUZZ_BASE_URL + BUZZ_HISTORY_URL;
     }
